caf61f1a829cd5737e7f0b2305270a0d18659467
Hollis Lau <hollislau@hotmail.com>
2015-10-01 11:40:02 -0700

    Refactor all methods to provide a simpler interface for users

--- js/main.js
+++ js/main.js
@@ -1,76 +1,15 @@
-"use strict"
+"use strict";
 
-var FizzBuzz = (function() {
-  var _outputStringsArray = [];
+(function() {
 
-  var FizzyBuzzy = function(string1, string2) {
-    _outputStringsArray.push(string1, string2);
+  var fizzBuzz = new FizzBuzz("Bull", "Shit");
+
+  function handler(e) {
+    e.preventDefault();
+    fizzBuzz.read(document.getElementById("start-value"), document.getElementById("end-value"));
+    fizzBuzz.write(document.getElementById("results"));
   }
 
-  FizzyBuzzy.prototype.getStrings = function() {
-    return _outputStringsArray;
-  }
-
-  FizzyBuzzy.prototype.read = function() {
-    var startValue = parseInt(document.getElementById("start-value").value);
-    var endValue = parseInt(document.getElementById("end-value").value);
-    var userInput = [];
-
-    userInput.push(startValue, endValue);
-
-    return userInput;
-  }
-
-  FizzyBuzzy.prototype.getOutput = function(inputs, strings) {
-    return _generateOutput(inputs, strings);
-  }
-
-  FizzyBuzzy.prototype.write = function(node, output) {
-    var table = node;
-    var newTableHead = document.createElement("thead");
-    var newTableBody = document.createElement("tbody");
-    var newTableHeaders = document.createElement("th");
-    var newTableRow = document.createElement("tr");
-
-    if (table.firstChild.nextSibling) {
-      this.clearTable(node);
-    }
-
-    table.appendChild(newTableHead).className = "clear";
-    table.lastChild.appendChild(newTableRow).appendChild(newTableHeaders).textContent = "#1";
-    table.appendChild(newTableBody).className = "clear";
-
-    for(var i = 0; i < output.length; i++) {
-      var newTableRow = document.createElement("tr");
-      var newTableData = document.createElement("td");
-
-      table.lastChild.appendChild(newTableRow).appendChild(newTableData)
-           .textContent = output[i];
-    }
-  }
-
-  FizzyBuzzy.prototype.clearTable = function(node) {
-    var table = node;
-    var clearElements = document.getElementsByClassName("clear");
-
-    for (var i = 0; i < clearElements.length;) {
-      table.removeChild(clearElements[0]);
-    }
-  }
-
-  return FizzyBuzzy;
+  document.getElementById("user-input").addEventListener("submit", handler, false);
 
 })();
-
-var fizzBuzz = new FizzBuzz("Fizz", "Buzz");
-
-document.getElementById("user-input").addEventListener("submit", _handler, false);
-
-function _handler(e) {
-  var userInput = fizzBuzz.read();
-  var outputStrings = fizzBuzz.getStrings();
-  var outputArray = fizzBuzz.getOutput(userInput, outputStrings);
-
-  e.preventDefault();
-  fizzBuzz.write(document.getElementById("results"), outputArray);
-}
