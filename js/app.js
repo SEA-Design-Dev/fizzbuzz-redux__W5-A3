20ca3b0a6705042e5b4ca102569c4bc5811789e1
Molly Kent <kent.molly@gmail.com>
2015-10-02 10:13:23 -0700

    Install npm module of fizzbuzz, modify app.js to restore DOM render

--- js/app.js
+++ js/app.js
@@ -1,27 +1,45 @@
 'use strict';
+
+var FizzyBuzzy = require('sea-d44-fizz-buzz-mk');
+
 (function() {
-  // create an instance on page load
-  var fizzbuzz = new FizzyBuzzy('Fla','Floo');  
-  // create variable for submission element
+  var word1 = 'Fla';
+  var word2 = 'Floo';
+  var fizzbuzz = new FizzyBuzzy(word1,word2);  
   var elSubmit = document.getElementById('goForFizzBuzz');
   
   // render function
-  function renderFizzBuzz() {
-    // store beginning and ending values in variables
+  function renderFizzBuzz(destination) {
     var startValue = parseInt(document.getElementById('beginValue').value);
     var stopValue = parseInt(document.getElementById('endValue').value);
-    // call the read & write functions
-    fizzbuzz.read(startValue,stopValue);
-    fizzbuzz.write(document.getElementById('fizzbuzz'));
-    // clear the entered values
+    fizzbuzz.input(startValue,stopValue);
+
+    while (destination.firstChild) {
+      destination.removeChild(destination.firstChild);
+    }
+    if (startValue < stopValue) {
+      var heading = document.createElement('h2');
+      heading.appendChild(document.createTextNode('Your ' + word1 + word2 + ' from ' + startValue + ' to ' + stopValue + ':'));
+      destination.appendChild(heading);
+      var newList = document.createElement('ul');
+      destination.appendChild(newList);
+    }
+    for (var i = 0; i < fizzbuzz.output().length; i++) {
+      var resultItem = document.createElement('li');
+      resultItem.appendChild(document.createTextNode(fizzbuzz.output()[i]));
+      newList.appendChild(resultItem);
+    }
+    
+    
     document.getElementById('beginValue').value = '';
     document.getElementById('endValue').value = '';
+
   };
 
   // event listener
   elSubmit.addEventListener('click', function(evt) {
     evt.preventDefault();
-    renderFizzBuzz();
+    renderFizzBuzz(document.getElementById('fizzbuzz'));
   });
 
 })();
