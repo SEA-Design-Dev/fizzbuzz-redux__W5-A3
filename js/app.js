4fe60b5f954dfc57190b1d781614c1935716b800
Chris Holt <christopher.d.holt@gmail.com>
2015-10-03 15:09:24 -0700

    created new/updated function for writing to the dom and tested if works

--- js/app.js
+++ js/app.js
@@ -1,12 +1,41 @@
+// var fizzbuzz = require('sea-d44-fizz-buzz-ch');
+
 function runGame (name, lowId, highId, divId) {
-  var game = name;
-  var lowNum = parseInt(document.getElementById(lowId).value);
-  var highNum = parseInt(document.getElementById(highId).value);
-  game.getNums(lowNum, highNum);
-  game.renderFB(divId);
+  var name, lowNum, highNum, id, print;
+  game = name;
+  lowNum = parseInt(document.getElementById(lowId).value);
+  highNum = parseInt(document.getElementById(highId).value);
+  id = divId;
+
+  writeNums = document.getElementById(id);
+  writeNums.innerHTML = "";
+  var heading = document.createElement("h2");
+  var hText = "<em>You chose the numbers " + lowNum + " and " + highNum + ".</em>";
+  heading.innerHTML = hText;
+  writeNums.appendChild(heading);
+
+  if (game.input().length > 0) {
+    game.input().length = 0;
+  }
+
+  game.input(lowNum, highNum);
+  print = game.output();
+
+  var modifyArray = function() {
+    var results = '';
+
+    for (i = 0; i < print.length; i++) {
+      results += print[i] + ' ';
+    }
+    return results;
+  };
+
+  writeFB = document.createElement("p");
+  writeFB.innerHTML = modifyArray();
+  writeNums.appendChild(writeFB);
 }
 
-var myFizzBuzz = new FizzBuzz("Shmizz", "Shmuzz");
+var myFizzBuzz = new fizzbuzz("Shmizz", "Shmuzz");
 
 var formId = document.getElementById("numbers");
 formId.addEventListener("submit", function(e) {
