9aacb03e791e1ed37d6e6e22b1b4fa397ded5d82
Zachary Gordon <z.gordon.h@gmail.com>
2015-10-05 11:10:43 -0700

    Add comments

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ var NodeBuzz = require('sea-d44-fizz-buzz-zg');
 
 
 document.getElementById("button-b").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // instantiated my new NodeBuzz function.
   var myfizz = new NodeBuzz('bleep', 'blorp');
 
+  // Assigning the input values to variables.
   aVal  = parseInt(document.getElementById("min").value);
   zVal  = parseInt(document.getElementById("max").value);
-
   myfizz.input(aVal, zVal);
 
-  document.getElementById("write").innerHTML="";
+  // Creating and appending DOM elements with processed inputs
+  document.getElementById("DOM-elements").innerHTML="";
   var newEl = document.createElement("p");
   var newtext = document.createTextNode(myfizz.output(this.result));
   newEl.appendChild(newtext);
-  document.getElementById("write").appendChild(newEl);
-  e.preventDefault();
+  document.getElementById("DOM-elements").appendChild(newEl);
 });
 
 
